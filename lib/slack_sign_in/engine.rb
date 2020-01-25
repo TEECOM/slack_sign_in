392b5e763e205cfee74d9f9e75fb6e4bf232c424
Tommy Schaefer <tommy.schaefer@teecom.com>
2020-01-25 13:18:11 -0600

    Automatically mount in application routes
    This change updates the engine to automatically be mounted in the
    end-user's application routes. It exposes a configuration for the
    mounting root, and defaults to "slack_sign_in."

--- lib/slack_sign_in/engine.rb
+++ lib/slack_sign_in/engine.rb
@@ -13,5 +13,11 @@ module SlackSignIn
         SlackSignIn.scopes = config.slack_sign_in.scopes || SlackSignIn::DEFAULT_SCOPES
       end
     end
+
+    initializer "slack_sign_in.mount" do |app|
+      app.routes.prepend do
+        mount SlackSignIn::Engine, at: app.config.slack_sign_in.root || "slack_sign_in"
+      end
+    end
   end
 end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,2 @@
 Rails.application.routes.draw do
-  mount SlackSignIn::Engine => "/slack_sign_in"
 end
