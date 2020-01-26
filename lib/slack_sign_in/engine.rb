2fa25bc29dfd55f1ba83d74c4520156ff615c855
Tommy Schaefer <tommy.schaefer@teecom.com>
2020-01-25 20:27:01 -0600

    Allow client ID / secret to be set in credentials

--- lib/slack_sign_in/engine.rb
+++ lib/slack_sign_in/engine.rb
@@ -8,8 +8,8 @@ module SlackSignIn
 
     initializer "slack_sign_in.config" do |app|
       config.after_initialize do
-        SlackSignIn.client_id = config.slack_sign_in.client_id
-        SlackSignIn.client_secret = config.slack_sign_in.client_secret
+        SlackSignIn.client_id = config.slack_sign_in.client_id || app.credentials.dig(:slack_sign_in, :client_id)
+        SlackSignIn.client_secret = config.slack_sign_in.client_secret || app.credentials.dig(:slack_sign_in, :client_secret)
         SlackSignIn.scopes = config.slack_sign_in.scopes || SlackSignIn::DEFAULT_SCOPES
       end
     end
